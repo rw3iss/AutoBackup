f0e9bee7e730fbd82082758867fa52777ebddccc
Ryan Weiss <rw3iss@gmail.com>
2015-11-13 00:01:28 -0500

    Initial

---
+++ AutoBackup/backup_script.php
@@ -0,0 +1,50 @@
+<?php
+//assume running from command line
+
+require_once "config.php";
+
+global $backups;
+global $backupDestination;
+
+//go through each backup location, copy to destination
+foreach($backups as $bl) {
+	$fullBackupDestination = $backupDestination . $bl;
+	
+	if(is_dir($bl)) {
+		ensure_directory($fullBackupDestination);
+		recursive_copy_directory($bl, $fullBackupDestination);
+	} else {
+		$destinationDirectory = dirname($fullBackupDestination);
+		ensure_directory($destinationDirectory);
+		//ensure_directory($destinationDirectory);
+		echo "\nCopying: " . $bl;
+		echo "\nTo: " . $fullBackupDestination;
+		copy($bl, $fullBackupDestination);
+	}
+
+	echo "\nBacked up: " . $fullBackupDestination;
+}
+
+function ensure_directory($dir) {
+	if (!file_exists($dir)) {
+	    mkdir($dir, 0777, true);
+	}
+}
+
+function recursive_copy_directory($src,$dst) { 
+    $dir = opendir($src); 
+    @mkdir($dst); 
+    while(false !== ( $file = readdir($dir)) ) { 
+        if (( $file != '.' ) && ( $file != '..' )) { 
+            if ( is_dir($src . '/' . $file) ) { 
+                recursive_copy_directory($src . '/' . $file,$dst . '/' . $file); 
+            } 
+            else { 
+                copy($src . '/' . $file,$dst . '/' . $file); 
+            } 
+        } 
+    } 
+    closedir($dir); 
+} 
+
+?>
---
+++ AutoBackup/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$backups = array(
+	"/Users/rw3iss/Library/Preferences/Klok2.DD7F2188B985C2439837C76B42A187050457E61B.1/Local Store/klok.db",
+	"/usr/local/var/mysql",
+	"/Users/rw3iss/Library/Containers/com.apple.Notes/Data/Library/Notes/"
+);
+
+$backupDestination = "/Volumes/SD 16GB/_OSX_AUTO_BACKUP";
