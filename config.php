49f4181b51921246b983584d94db306721e10dd9
Ryan Weiss <rw3iss@Ryans-MacBook-Pro.local>
2015-11-29 23:04:49 -0500

    default message

--- config.php
+++ config.php
@@ -2,10 +2,8 @@
 
 $backups = array(
 	"/Users/rw3iss/Library/Preferences/Klok2.DD7F2188B985C2439837C76B42A187050457E61B.1/Local Store/klok.db",
-	"/usr/local/var/mysql",
-	"/Users/rw3iss/Library/Containers/com.apple.Notes/Data/Library/Notes/",
-	"/Users/rw3iss/.aliases",
-	"/Users/rw3iss/.bash_profile"
+//	"/usr/local/var/mysql",
+	"/Users/rw3iss/Library/Containers/com.apple.Notes/Data/Library/Notes/"
 );
 
-$backupDestination = ""; //change this to your backup path
+$backupDestination = "/Volumes/SD 64GB/_OSX_AUTO_BACKUP";
